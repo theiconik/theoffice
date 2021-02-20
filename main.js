41d6004dc53c70258ce2e22589e1aa66346a069f
theiconik <kumarujjawal51@gmail.com>
2021-02-21 01:30:29 +0530

    Navigation Header built

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function toggleMenu() {
+   const menuToggle = document.querySelector('.toggle');
+   menuToggle.classList.toggle('active');
+   const navigation = document.querySelector('.navigation');
+   navigation.classList.toggle('active');
+}
