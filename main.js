771ab271f9d9c120f0ef3169159217ecee571e42
theiconik <kumarujjawal51@gmail.com>
2021-02-21 14:23:36 +0530

    Active class to home images

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 
+let imgBox = document.querySelectorAll('.imgBox');
+imgBox.forEach(popup => popup.addEventListener('click', () => {
+   popup.classList.toggle('active');
+}))
+
 function toggleMenu() {
    const menuToggle = document.querySelector('.toggle');
    menuToggle.classList.toggle('active');
